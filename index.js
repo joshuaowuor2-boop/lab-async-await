7a24ba9d14e2a4c2ba98dd0a319a06c7b601bf8a
joshuaowuor2-boop <joshua.owuor2@student.moringaschool.com>
2026-04-26 14:09:05 +0300

    fix:correct variablename

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 async function fetchData(document) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const posts = await response.json();
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const data = await response.json();
 
-  posts.forEach(post => {
     const h1 = document.createElement('h1');
     const p = document.createElement('p');
 
-    h1.textContent = post.title;
-    p.textContent = post.body;
+    h1.textContent = data.title;
+    p.textContent = data.body;
 
     document.body.appendChild(h1);
     document.body.appendChild(p);
-  });
-}
+  }
 
 module.exports = fetchData;
